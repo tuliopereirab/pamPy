75dd8273530653173f3d08ffc11c8beae88a8e80
Tulio <tulio.pereirab@gmail.com>
2019-10-13 19:35:48 -0300

    Added the control unit into the top entity and inserted the new control signals.

--- CONTROL_UNIT.v
+++ CONTROL_UNIT.v
@@ -1,7 +1,6 @@
 module CONTROL_UNIT #(
     parameter DATA_WIDTH = 8,
     parameter ADDR_WIDTH = 12,
-    parameter ULA_WIDTH = 24,
     parameter INSTRUCTION_WIDTH = 16
     ) (
         //input
@@ -11,11 +10,16 @@ module CONTROL_UNIT #(
         input [(ADDR_WIDTH-1):0] TOS_IN,
         //outputs
         output reg CTRL_REG_TOS_FUNCTION, SEL_SOMADOR_SUBTRATOR, CTRL_STACK_FUNCTION, CTRL_REG_DATA_RETURN,
-        output reg [(DATA_WIDTH-1):0] STACK_OUT,
+        output reg SEL_PC_UPDATER, SEL_TOS_UPDATER,
+        output reg CTRL_STACK, CTRL_MEM_EXT,
         output reg CTRL_REG_OP1, CTRL_REG_OP2,
         output reg CTRL_STACK_COMP,
         output reg [3:0] SEL_ULA,
-        output reg CTRL_REG_ARG, CTRL_REG_INSTR, CTRL_REG_JUMP, CTRL_REG_PC, SEL_MUX_PC
+        output reg CTRL_REG_READ_STACK, CTRL_REG_WRITE_STACK,
+        output reg CTRL_REG_READ_MEMORY, CTRL_REG_WRITE_MEMORY,
+        output reg CTRL_REG_ARG, CTRL_REG_INSTR, CTRL_REG_JUMP, CTRL_REG_PC,
+        output reg SEL_MUX_TOS,
+        output reg [1:0] SEL_MUX_PC
     );
 
 
